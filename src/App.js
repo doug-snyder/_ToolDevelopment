02065f846cb23988c2d56f796270b221e50304a3
douglas-snyder <mistershake2099@gmail.com>
2023-07-31 09:28:26 -0700

    Add dropdowns. Arrange elements.

--- src/App.js
+++ src/App.js
@@ -13,17 +13,42 @@ function PageTitle() {
 
 function OriginSection() {
   return (
-    <div class="">
+    <div className="subsection">
       <h3>ORIGIN</h3>
+      <div className="dropdownSection">
+        Departure Time:
+        <CreateDropdown />
+      </div>
+      <div className="dropdownSection">
+        Departure Station:
+        <CreateDropdown />
+      </div>
     </div>
   );
 }
 
 function DestinationSection() {
   return (
-    <div class="">
+    <div className="subsection">
       <h3>DESTINATION</h3>
+      <div className="dropdownSection">
+        Arrival Time:
+        <CreateDropdown />
+      </div>
+      <div className="dropdownSection">
+        Arrival Station:
+        <CreateDropdown />
+      </div>
     </div>
+  );
+}
+
+function CreateDropdown() {
+  return (
+    <select className="dropdownField">
+      <option value="optionOne">- - -</option>
+      <option value="optionTwo">- - -</option>
+    </select>
   );
 }
 
